c336ecfe4183a7c7de3342fa1b3e0f1f9be52c71
Akmal070 <akmal.9807@mail.ru>
2021-03-06 16:38:21 +0500

    Created Folders and modules

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const articleRouter = require('./routes/articles')
 const app = express()
-const port = 3000 || process.env
 
-app.get('/', (req, res) => res.send('Hello World!'))
+mongoose.connect('mongodb://localhost/blog', { useUnifiedTopology: true, useNewUrlParser: true })
+
+app.set('view engine', 'ejs')
+
+app.use('/articles', articleRouter)
+app.use
+
+app.get('/', (req, res) => {
+    const articles = [{
+        title: 'Test Article',
+        createdAt: new Date(),
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum, dolores pariatur. Laborum odio laboriosam, numquam, nesciunt soluta temporibus ratione amet aspernatur veniam quo beatae vitae facilis repudiandae voluptatem ex corrupti.'
+    }]
+    res.render('articles/index', {articles})
+})
+
+const port = 8000 || process.env
 app.listen(port, () => console.log(`Example app listening on port http://127.0.0.1:${port}`))
